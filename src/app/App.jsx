547f47c2e808952dd6ec477dce08a84c094206f0
Manish Manker <manish.manker@pixelnx.com>
2026-02-06 19:30:01 +0530

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+      <h1>App</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
